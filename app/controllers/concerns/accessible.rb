7a3b4eb26c3dd4231ba519cdff2153912844f9c5
Kachavi <vishaldineshbhaikacha@gmail.com>
2024-04-12 23:37:07 +0530

    added auth

---
+++ app/controllers/concerns/accessible.rb
@@ -0,0 +1,23 @@
+# ../controllers/concerns/accessible.rb
+module Accessible
+  extend ActiveSupport::Concern
+  included do
+    before_action :check_user
+  end
+
+  protected
+  def check_user
+    if current_admin
+      flash.clear
+      # if you have rails_admin. You can redirect anywhere really
+      # redirect_to(root_path) and return
+    elsif current_student
+      flash.clear
+      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+      redirect_to root_path, notice: "Not accessible" and return 
+    elsif current_teacher
+      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+      redirect_to root_path, notice: "Not accessible" and return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :students, path: 'students', controllers: {
+    sessions: "students/sessions",
+    registrations: "students/registrations"
+  }
+
+  devise_for :teachers, path: 'teachers', controllers: {
+    sessions: "teachers/sessions",
+    registrations: "teachers/registrations"
+  }
+
+  devise_for :admins, path: 'admins', controllers: {
+    sessions: "admins/sessions",
+    registrations: "admins/registrations"
+  }
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
