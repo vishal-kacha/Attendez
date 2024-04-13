62c080797b178b26a35e6a013bb11656d478fb54
Kachavi <vishaldineshbhaikacha@gmail.com>
2024-04-13 15:10:22 +0530

    db seeding dummy data

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -9,12 +9,12 @@ module Accessible
   def check_user
     if current_admin
       flash.clear
-      redirect_to root_path, notice: "Sucessfully loged in" and return 
+      redirect_to root_path and return 
     elsif current_student
       flash.clear
-      redirect_to root_path, notice: "Sucessfully loged in" and return 
+      redirect_to root_path and return 
     elsif current_teacher
-      redirect_to root_path, notice: "Sucessfully loged in" and return
+      redirect_to root_path and return
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,54 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create an admin user
+admin_email = 'admin@example.com'
+admin_password = 'password'
+Admin.create!(email: admin_email, password: admin_password, password_confirmation: admin_password)
+
+# Create a branch
+branch = Branch.create!(name: 'cs')
+
+# Create subjects (assuming 7 subjects)
+subjects_data = [
+  { subject_name: 'Java', branch_id: 1},
+  { subject_name: 'R', branch_id:  1},
+]
+
+subjects_data.each do |subject_attrs|
+  Subject.create!(subject_attrs)
+end
+
+
+# Create teachers (assuming 2 teachers)
+teacher1 = Teacher.create!(email: 'teacher1@gmail.com', password: "teacher", branch_id: 1, subject_id: 1)
+teacher2 = Teacher.create!(email: 'teacher2@gmail.com', password: "teacher", branch_id: 1, subject_id: 2)
+
+# Create students (assuming 5 students)
+students_data = [
+  { email: 'student1@example.com', password: "student", branch_id: 1 },
+  { email: 'student2@example.com', password: "student", branch_id: 1 },
+  { email: 'student3@example.com', password: "student", branch_id: 1 },
+  { email: 'student4@example.com', password: "student", branch_id: 1 },
+  { email: 'student5@example.com', password: "student", branch_id: 1 }
+]
+
+students_data.each do |student_attrs|
+  Student.create!(student_attrs)
+end
+
+Attendance.create(subject_id: 1, student_id: 1, status: true)
+Attendance.create(subject_id: 2, student_id: 1, status: true)
+
+Attendance.create(subject_id: 1, student_id: 2, status: true)
+Attendance.create(subject_id: 2, student_id: 2, status: true)
+
+Attendance.create(subject_id: 1, student_id: 3, status: true)
+Attendance.create(subject_id: 2, student_id: 3, status: true)
+
+Attendance.create(subject_id: 1, student_id: 4, status: true)
+Attendance.create(subject_id: 2, student_id: 4, status: true)
+
+Attendance.create(subject_id: 1, student_id: 5, status: true)
+Attendance.create(subject_id: 2, student_id: 5, status: true)
